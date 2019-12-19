4d0181f8887fbe4edb5860ea12645d2394698fb0
cbayonao <drcamilob@live.com>
2019-12-19 21:09:58 +0000

    Answer 1 Attemp 0

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    cpy = []
+    for a in range(len(my_list)):
+        if my_list[a] != search:
+            cpy.append(my_list[a])
+        else:
+            cpy.append(replace)
+    return cpy
