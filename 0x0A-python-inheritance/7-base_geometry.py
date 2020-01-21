623da859a8c390bd1d527ce17c126688c5a0e652
cbayonao <drcamilob@live.com>
2020-01-20 22:06:56 -0500

    7.0

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+Public instance method: def area(self):
+raises an Exception with the message area() is not implemented
+Public instance method: def integer_validator(self, name, value):
+if value is not an integer: raise a TypeError exception,
+with the message <name> must be an integer
+if value is less or equal to 0: raise a ValueError exception
+with the message <name> must be greater than 0
+"""
+class BaseGeometry:
+    def area(self):
+        raise Exception(area() is not implemented)
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
