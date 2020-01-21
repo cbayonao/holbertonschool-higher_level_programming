e1546e5aac227d59ea274700ad6243fd1161d00f
cbayonao <drcamilob@live.com>
2020-01-20 23:21:16 -0500

    11.0

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+    def __str__(self):
+        return "[{}] {}/{}".format(self.__class__.__name__, self.__size,
+                                   self.__size)
+    def area(self):
+        return self.__size * self.__size
+
+
