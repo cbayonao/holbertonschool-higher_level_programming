6bbdaf17cea0d408a12b837221f6ac0deec80155
cbayonao <drcamilob@live.com>
2020-01-21 17:30:21 -0500

    6.1

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -3,9 +3,9 @@
 Function that returns an object (Python data structure)
 represented by a JSON string
 """
-
-
 import json
+
+
 def from_json_string(my_str):
     frmjson = json.loads(my_str)
     return frmjson
