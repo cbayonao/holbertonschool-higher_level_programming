6b6cfab9e97296f9fd56b3548fec608478f16b8e
cbayonao <drcamilob@live.com>
2020-01-20 21:49:47 -0500

    6.0

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+Public instance method: def area(self):
+raises an Exception with the message area() is not implemented
+"""
+class BaseGeometry:
+    def area(self):
+        raise Exception('area() is not implemented')
