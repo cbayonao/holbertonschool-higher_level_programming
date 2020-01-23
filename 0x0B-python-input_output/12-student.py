36a2836545a750ef295b8451f5ac29a6e4502b0a
cbayonao <drcamilob@live.com>
2020-01-22 22:28:59 -0500

    12.0

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self, attrs=None):
+        dic = dict()
+        if attrs:
+            for i in range(0, len(attrs)):
+                if attrs[i] in self.__dict__:
+                    dic[attrs[i]] = self.__dict__[attrs[i]]
+        if attrs is None:
+            dic = self.__dict__
+        return dic
