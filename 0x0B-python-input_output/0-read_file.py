47904cbd3c882fbfd12854cade069827ebd310f7
cbayonao <drcamilob@live.com>
2020-01-22 22:30:59 -0500

    fix 0 and 12

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,3 @@ def read_file(filename=""):
     with open(filename) as f:
         txt = f.read()
         print(txt, end='')
-
--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -8,6 +8,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         dic = dict()
         if attrs:
