cbcc96e5dc8a7d7e0bbb2d681362c28652b58fcf
cbayonao <drcamilob@live.com>
2020-01-27 23:13:34 -0500

    11.0

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,3 +32,19 @@ def __str__(self):
         s = "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.__width)
         return s
+
+@property
+def size(self):
+    """Retrieves the size attribute."""
+    return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size attribute."""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
