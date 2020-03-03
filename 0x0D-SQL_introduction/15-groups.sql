af32bd303da79af5aebe1699c9d07d50a1943958
cbayonao <drcamilob@live.com>
2020-03-02 20:21:09 -0500

    [Feature Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.]

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- Script that lists all records of the table second_table of the
+-- database hbtn_0c_0 in your MySQL server.
+SELECT score, COUNT(score) AS number FROM second_table
+GROUP BY score
+ORDER BY number DESC;
