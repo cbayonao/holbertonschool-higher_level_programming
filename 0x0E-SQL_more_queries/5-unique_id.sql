36c5444ff4fa26ebd271f5e0f11df8a79eaff3fc
cbayonao <drcamilob@live.com>
2020-03-04 02:10:39 +0000

    [Fix] Write a script that creates the table unique_id on your MySQL server.

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,3 @@
 -- Write a script that creates the table unique_id on your MySQL server.
-CREATE TABLE IF NOT EXISTS unique_id (
-       id INT UNIQUE,
-       name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE,
+name VARCHAR(256));
