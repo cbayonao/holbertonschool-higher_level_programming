cc0652f72686d349ca4d2dc863c95ac2f8b88070
cbayonao <drcamilob@live.com>
2020-03-29 10:12:27 -0500

    [Fix] Pep8 Style

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     from sqlalchemy.orm import Session
     """This script should take 3 arguments: mysql username, mysql password
     and database name"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                           sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = Session(engine)
+    en = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                       sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(en)
+    session = Session(en)
     """Results must be sorted in ascending order by states.id"""
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
