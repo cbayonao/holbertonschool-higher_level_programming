6388395e4e35fb246ffea60798e75397505f6141
cbayonao <drcamilob@live.com>
2020-03-29 17:19:50 -0500

    [Feature] Answer for question 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa.
+"""
+if __name__ == "__main__":
+    try:
+        import sys
+        from model_state import Base, State
+        from sqlalchemy import create_engine
+        from sqlalchemy.orm import Session
+        """This script should take 3 arguments: mysql username, mysql password
+        and database name"""
+        eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                            .format(sys.argv[1], sys.argv[2],
+                                    sys.argv[3]), pool_pre_ping=True)
+
+        Base.metadata.create_all(eng)
+        session = Session(eng)
+        for state_to_del in session.query(State).filter
+        (State.name.like('%a%')):
+            session.delete(state_to_del)
+        session.commit()
+        session.close()
+    except:
+        print("Can't connect to database")
