9dda681dc5eea9332eb4f883bcbc8cd12dffc644
cbayonao <drcamilob@live.com>
2020-03-29 10:03:33 -0500

    [Feature] Answer for 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+"""This script should take 3 arguments: mysql username, mysql password
+and database name"""
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                       sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+session = Session(engine)
+"""Results must be sorted in ascending order by states.id"""
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
