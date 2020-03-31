ad005bf1bd8e4537d70edf9da10c7a97c10d3976
cbayonao <drcamilob@live.com>
2020-03-31 14:15:23 -0500

    [Feature] Advanced task 15

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,12 +5,13 @@ an instance Base = declarative_base()
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from relationship_state import Base
+Base = declarative_base()
 
 
 class City(Base):
     """Class City"""
     __tablename__ = 'cities'
-    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    id = Column(Integer, nullable=False, unique=True, autoincrement=True,
+                primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -6,13 +6,15 @@ an instance Base = declarative_base()
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-
+from relationship_city import City, Base
 Base = declarative_base()
 
 
 class State(Base):
     """Class State"""
     __tablename__ = 'states'
-    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    id = Column(Integer, nullable=False, unique=True, autoincrement=True,
+                primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="states")
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
