975a85a2ab90d9940f8cccf51e553aeb7e83ebd7
cbayonao <drcamilob@live.com>
2020-04-10 11:06:27 -0500

    [Answer 6]

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that fetches URL
+"""
+
+def fetch():
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as respuesta:
+        html = respuesta.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+
+
+if __name__ == "__main__":
+    fetch()
