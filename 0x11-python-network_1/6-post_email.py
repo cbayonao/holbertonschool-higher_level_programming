b5442e87b928ef2590429561c8b86a238e1099e0
cbayonao <drcamilob@live.com>
2020-04-19 11:35:35 -0500

    [Feature]POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+
+def post_request():
+    import requests
+    import sys
+
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
+
+
+if __name__ == "__main__":
+    post_request()
