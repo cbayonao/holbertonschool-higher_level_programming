cdeaacff5c02c9ba65ad548412fe41e4ebf91c35
cbayonao <drcamilob@live.com>
2020-05-05 13:40:30 -0500

    [Another way]

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function myFunction () {
-    let myVar = 'C is fun';
-    console.log(myVar);
-    myVar = 'Python is cool';
-    console.log(myVar);
-    myVar = 'Javascript is amazing';
-    console.log(myVar);
+  let myVar = 'C is fun';
+  console.log(myVar);
+  myVar = 'Python is cool';
+  console.log(myVar);
+  myVar = 'Javascript is amazing';
+  console.log(myVar);
 }
 myFunction();
