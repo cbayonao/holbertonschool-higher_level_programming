444d97ee37bf9646a8c19bb3f6089c47da5158f2
cbayonao <drcamilob@live.com>
2020-05-05 12:35:26 -0500

    [Feature] 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function myFunction () {
+  const myVar1 = 'C is fun';
+  console.log(myVar1);
+  const myVar2 = 'Python is cool';
+  console.log(myVar2);
+  const myVar3 = 'Javascript is amazing';
+  console.log(myVar3);
+}
+myFunction();
