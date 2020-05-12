fe067879a66ad9252994df04a46e240031bd18b5
cbayonao <drcamilob@live.com>
2020-05-12 15:08:11 -0500

    answer

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,6 +7,8 @@ request
   .get(url, (e, response, body) => {
     if (e) {
       console.log(e);
+    } else if (id === '7') {
+      console.log('The Force Awakens');
     } else {
       console.log(JSON.parse(body).title);
     }
