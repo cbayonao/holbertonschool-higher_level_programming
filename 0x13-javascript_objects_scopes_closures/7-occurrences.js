f05986a462a4090baa4e8e3aec61da4e0cc0901e
cbayonao <drcamilob@live.com>
2020-05-11 21:56:56 -0500

    [Feature] Occurrenses Write a function that returns the number of occurrences in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let cont = 0;
-  size = list.length;
+  const size = list.length;
   for (let i = 0; i < size; i++) {
     if (searchElement === list[i]) {
       cont++;
     }
   }
   return cont;
-}
+};
